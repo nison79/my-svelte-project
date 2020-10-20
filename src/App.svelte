950fe4cc1b6a75ce3ef05c140c1b990f244c318c
nison79 <georgenison@gmail.com>
2020-10-20 19:55:27 +0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,51 @@
+<script>
+	import Navbar from './Navbar.svelte';
+	import Player from './Player.svelte';
+	import AddPlayer from './AddPlayer.svelte';
+
+	let players = [
+		{
+			name: "Nick Baferas",
+			points: 45
+		},
+		{
+			name: "George Parasxidis",
+			points: 89 
+		},
+		{
+			name: "George Nikoglou",
+			points: 56
+		},
+	];
+
+	const addPlayer = (e) => {
+		const newPlayer = e.detail;
+		players=[...players,newPlayer];
+	};
+
+	const removePlayer = e => {
+		players = players.filter(player => player.name !== e.detail);
+	};
+</script>
+
+<main>
+	<Navbar />
+	<div class="container">
+		<AddPlayer on:addplayer={addPlayer} />
+		{#if players.length === 0}
+			<p>No Players</p>
+		{:else}
+			{#each players as player }
+				<Player name={player.name} points={player.points} on:removeplayer={removePlayer} />
+			{/each}
+		{/if}
+	</div>
+
+
+</main>
+
+<style>
+	
+
+
+</style>
